17b1d6d5edb25115c0023227323c386fee78b364
shug <vxzyfx@gmail.com>
2024-09-19 23:08:08 +0800

    feat: runtime signal

--- tokio/src/runtime/signal/mod.rs
+++ tokio/src/runtime/signal/mod.rs
@@ -18,14 +18,20 @@ use std::time::Duration;
 #[derive(Debug)]
 pub(crate) struct Driver {
     /// Thread parker. The `Driver` park implementation delegates to this.
+    /// 用于管理信号驱动器的事件循环和阻塞机制.
+    /// 信号驱动器的 park 和 park_timeout 方法将委托给这个 io::Driver 来处理线程阻塞和唤醒.
     io: io::Driver,
 
     /// A pipe for receiving wake events from the signal handler
+    /// 信号处理程序将信号发送到这个管道中,
+    /// 而信号驱动器通过读取该管道来处理这些信号
     receiver: UnixStream,
 
     /// Shared state. The driver keeps a strong ref and the handle keeps a weak
     /// ref. The weak ref is used to check if the driver is still active before
     /// trying to register a signal handler.
+    /// 用于管理信号驱动器的共享状态.
+    /// Arc 提供强引用,而 Handle 持有的是这个 Arc 的弱引用(Weak<()>).
     inner: Arc<()>,
 }
 
@@ -40,6 +46,7 @@ pub(crate) struct Handle {
 
 impl Driver {
     /// Creates a new signal `Driver` instance that delegates wakeups to `park`.
+    /// 创建一个新的信号`Driver`实例,将唤醒委托给`park`.
     pub(crate) fn new(io: io::Driver, io_handle: &io::Handle) -> std_io::Result<Self> {
         use std::mem::ManuallyDrop;
         use std::os::unix::io::{AsRawFd, FromRawFd};
@@ -71,6 +78,7 @@ impl Driver {
             ManuallyDrop::new(unsafe { std::os::unix::net::UnixStream::from_raw_fd(receiver_fd) });
         let mut receiver = UnixStream::from_std(original.try_clone()?);
 
+        // 将信号接收器注册到 I/O 驱动器中
         io_handle.register_signal_receiver(&mut receiver)?;
 
         Ok(Self {
@@ -105,24 +113,27 @@ impl Driver {
     fn process(&mut self) {
         // If the signal pipe has not received a readiness event, then there is
         // nothing else to do.
+        // 检查信号是否准备好
         if !self.io.consume_signal_ready() {
             return;
         }
 
         // Drain the pipe completely so we can receive a new readiness event
         // if another signal has come in.
+        // 完全排空管道,以便如果有另一个信号进来,我们可以收到新的就绪事件.
         let mut buf = [0; 128];
         #[allow(clippy::unused_io_amount)]
         loop {
             match self.receiver.read(&mut buf) {
-                Ok(0) => panic!("EOF on self-pipe"),
-                Ok(_) => continue, // Keep reading
-                Err(e) if e.kind() == std_io::ErrorKind::WouldBlock => break,
+                Ok(0) => panic!("EOF on self-pipe"), // 异常情况，因为信号管道不应该结束
+                Ok(_) => continue, // 信号事件只需要通知信号到达的事实,具体内容不重要.
+                Err(e) if e.kind() == std_io::ErrorKind::WouldBlock => break, // 当前没有更多数据可以读取,退出循环
                 Err(e) => panic!("Bad read on self-pipe: {}", e),
             }
         }
 
         // Broadcast any signals which were received
+        // 将信号广播给所有注册的信号监听器
         globals().broadcast();
     }
 }
